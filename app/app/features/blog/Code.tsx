883493593f55c0c49a3547fa3d1c15d67502a2f5
Simon Boisset <sboisset@ambuliz.com>
2022-04-19 09:21:59 +0200

    Customize Prism theme

--- app/app/features/blog/Code.tsx
+++ app/app/features/blog/Code.tsx
@@ -1,5 +1,109 @@
-import Highlight, { defaultProps } from 'prism-react-renderer';
+import Highlight, { defaultProps, PrismTheme } from 'prism-react-renderer';
 import { CodeBlock } from './types/blocks';
+
+var theme: PrismTheme = {
+  plain: {
+    color: '#d76a60',
+    backgroundColor: '#1E1E1E',
+  },
+  styles: [
+    {
+      types: ['prolog'],
+      style: {
+        color: 'rgb(0, 0, 128)',
+      },
+    },
+    {
+      types: ['comment'],
+      style: {
+        color: 'rgb(106, 153, 85)',
+      },
+    },
+    {
+      types: ['builtin', 'changed', 'keyword', 'interpolation-punctuation'],
+      style: {
+        color: '#9f54c4',
+      },
+    },
+    {
+      types: ['number', 'inserted'],
+      style: {
+        color: 'rgb(181, 206, 168)',
+      },
+    },
+    {
+      types: ['constant'],
+      style: {
+        color: '#d76a60',
+      },
+    },
+    {
+      types: ['attr-name', 'variable'],
+      style: {
+        color: 'rgb(222, 224, 116)',
+      },
+    },
+    {
+      types: ['deleted', 'string', 'attr-value', 'template-punctuation'],
+      style: {
+        color: 'rgb(124, 206, 120)',
+      },
+    },
+    {
+      types: ['selector'],
+      style: {
+        color: 'rgb(213, 182, 115)',
+      },
+    },
+    {
+      // Fix tag color
+      types: ['tag'],
+      style: {
+        color: 'rgb(223, 209, 60)',
+      },
+    },
+    {
+      // Fix tag color for HTML
+      types: ['tag'],
+      languages: ['markup'],
+      style: {
+        color: 'rgb(86, 156, 214)',
+      },
+    },
+    {
+      types: ['punctuation', 'operator'],
+      style: {
+        color: 'rgb(212, 212, 212)',
+      },
+    },
+    {
+      // Fix punctuation color for HTML
+      types: ['punctuation'],
+      languages: ['markup'],
+      style: {
+        color: '#808080',
+      },
+    },
+    {
+      types: ['function'],
+      style: {
+        color: 'rgb(67, 153, 218)',
+      },
+    },
+    {
+      types: ['class-name'],
+      style: {
+        color: 'rgb(223, 209, 60)',
+      },
+    },
+    {
+      types: ['char'],
+      style: {
+        color: 'rgb(209, 105, 105)',
+      },
+    },
+  ],
+};
 
 export default function Code({ caption, language, rich_text }: CodeBlock['code']) {
   return (
@@ -13,7 +117,11 @@ export default function Code({ caption, language, rich_text }: CodeBlock['code']
           </div>
         )}
 
-        <Highlight {...defaultProps} code={rich_text.map((text) => text.plain_text).join(' ')} language={language}>
+        <Highlight
+          {...defaultProps}
+          code={rich_text.map((text) => text.plain_text).join(' ')}
+          language={language}
+          theme={theme}>
           {({ tokens, getLineProps, getTokenProps }) => (
             <>
               <button
